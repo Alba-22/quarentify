e2dbcc7fe41a21b114f52f64e4b84ecfd8230eb6
Gustavo Alba <alba__22@outlook.com>
2020-07-25 20:17:24 -0300

    Added arrows to scroll horizontal lists

--- lib/components/top.artists.widget.dart
+++ lib/components/top.artists.widget.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:quarentify/components/top.artists.card.dart';
 import 'package:quarentify/models/top.artists.model.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
-class TopArtistsWidget extends StatelessWidget {
+class TopArtistsWidget extends StatefulWidget {
 
   final List<ArtistsItems> artists;
 
   TopArtistsWidget({Key key, @required this.artists}) : super(key: key);
 
-  final scrollController = new ScrollController();
-  
+  @override
+  _TopArtistsWidgetState createState() => _TopArtistsWidgetState();
+}
+
+class _TopArtistsWidgetState extends State<TopArtistsWidget> {
+
+  final ItemScrollController itemScrollController = ItemScrollController();
+  final ItemPositionsListener itemPositionsListener = ItemPositionsListener.create();
+
+  int actualIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,23 +31,70 @@ class TopArtistsWidget extends StatelessWidget {
               horizontal: MediaQuery.of(context).size.width * 0.05
             ),
             alignment: Alignment.centerLeft,
-            child: Text.rich(
-              TextSpan(
-                text: "Top ${artists.length} Artistas",
-                children: [
-                  TextSpan(
-                    text: "  (clique para ver o perfil)",
-                    style: TextStyle(
-                      fontSize: 12,
-                      letterSpacing: 0.5
-                    )
-                  )
-                ]
-              ),
-              style: TextStyle(
-                color: Theme.of(context).textSelectionColor,
-                fontSize: 20
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Top ${widget.artists.length} Artistas",
+                  style: TextStyle(
+                    color: Theme.of(context).textSelectionColor,
+                    fontSize: 20
+                  ),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      padding: EdgeInsets.all(0),
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          double size = MediaQuery.of(context).size.width - 0.1;
+                          actualIndex -= size ~/ 230;
+                          if (actualIndex >= 0) {
+                            itemScrollController.jumpTo(
+                              index: actualIndex,
+                            );
+                          }
+                          else {
+                            actualIndex += size ~/ 230;
+                          }
+                        });
+                      },
+                    ),
+                    Container(
+                      height: 25,
+                      width: 1.5,
+                      color: Colors.white,
+                    ),
+                    IconButton(
+                      padding: EdgeInsets.only(right: 0),
+                      icon: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          double size = MediaQuery.of(context).size.width - 0.1;
+                          actualIndex += size ~/ 230;
+                          if (actualIndex <= widget.artists.length) {
+                            itemScrollController.jumpTo(
+                              index: actualIndex,
+                            );
+                          }
+                          else {
+                            actualIndex -= size ~/ 230;
+                          }
+                        });
+                      },
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
           Container(
@@ -50,16 +107,18 @@ class TopArtistsWidget extends StatelessWidget {
           ),
           Container(
             height: 300,
-            child: ListView.builder(
+            child: ScrollablePositionedList.builder(
+              itemScrollController: itemScrollController,
+              itemPositionsListener: itemPositionsListener,
               scrollDirection: Axis.horizontal,
-              itemCount: artists.length,
+              itemCount: widget.artists.length,
               itemBuilder: (BuildContext context, int index) {
                 return Container(
                   margin: index == 0
                   ? EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.05)
                   : EdgeInsets.all(0),
                   child: TopArtistsCard(
-                    artist: artists[index],
+                    artist: widget.artists[index],
                   ),
                 );
               },
--- lib/components/top.tracks.widget.dart
+++ lib/components/top.tracks.widget.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:quarentify/components/top.tracks.card.dart';
 import 'package:quarentify/models/top.tracks.model.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
-class TopTracksWidget extends StatelessWidget {
+class TopTracksWidget extends StatefulWidget {
 
   final List<TracksItems> tracks;
 
-  const TopTracksWidget({Key key, @required this.tracks}) : super(key: key);
-  
+  TopTracksWidget({Key key, @required this.tracks}) : super(key: key);
+
+  @override
+  _TopTracksWidgetState createState() => _TopTracksWidgetState();
+}
+
+class _TopTracksWidgetState extends State<TopTracksWidget> {
+
+  final ItemScrollController itemScrollController = ItemScrollController();
+  final ItemPositionsListener itemPositionsListener = ItemPositionsListener.create();
+
+  int actualIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,24 +30,70 @@ class TopTracksWidget extends StatelessWidget {
             margin: EdgeInsets.symmetric(
               horizontal: MediaQuery.of(context).size.width * 0.05
             ),
-            alignment: Alignment.centerLeft,
-            child: Text.rich(
-              TextSpan(
-                text: "Top ${tracks.length} Músicas",
-                children: [
-                  TextSpan(
-                    text: "  (clique para ouvir a música)",
-                    style: TextStyle(
-                      fontSize: 12,
-                      letterSpacing: 0.5
-                    )
-                  )
-                ]
-              ),
-              style: TextStyle(
-                color: Theme.of(context).textSelectionColor,
-                fontSize: 20
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Top ${widget.tracks.length} Músicas",
+                  style: TextStyle(
+                    color: Theme.of(context).textSelectionColor,
+                    fontSize: 20
+                  ),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      padding: EdgeInsets.all(0),
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          double size = MediaQuery.of(context).size.width - 0.1;
+                          actualIndex -= size ~/ 230;
+                          if (actualIndex >= 0) {
+                            itemScrollController.jumpTo(
+                              index: actualIndex,
+                            );
+                          }
+                          else {
+                            actualIndex += size ~/ 230;
+                          }
+                        });
+                      },
+                    ),
+                    Container(
+                      height: 25,
+                      width: 1.5,
+                      color: Colors.white,
+                    ),
+                    IconButton(
+                      padding: EdgeInsets.only(right: 0),
+                      icon: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          double size = MediaQuery.of(context).size.width - 0.1;
+                          actualIndex += size ~/ 230;
+                          if (actualIndex <= widget.tracks.length) {
+                            itemScrollController.jumpTo(
+                              index: actualIndex,
+                            );
+                          }
+                          else {
+                            actualIndex -= size ~/ 230;
+                          }
+                        });
+                      },
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
           Container(
@@ -48,16 +106,18 @@ class TopTracksWidget extends StatelessWidget {
           ),
           Container(
             height: 300,
-            child: ListView.builder(
+            child: ScrollablePositionedList.builder(
+              itemScrollController: itemScrollController,
+              itemPositionsListener: itemPositionsListener,
               scrollDirection: Axis.horizontal,
-              itemCount: tracks.length,
+              itemCount: widget.tracks.length,
               itemBuilder: (BuildContext context, int index) {
                 return Container(
                   margin: index == 0
                   ? EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.05)
                   : EdgeInsets.all(0),
                   child: TopTracksCard(
-                    track: tracks[index],
+                    track: widget.tracks[index],
                   ),
                 );
               },
